8bf60957b69eade0c1d42a5aae527d612cb5fa24
TheDyingFlutchman <vanklaverenjoost@gmail.com>
2025-07-13 13:18:09 +0200

    Added parameter to bookbot

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-from stats import get_word_count
+from stats import *
+import sys
+
+def get_content(book):
+     with open(book) as f:
+        file_contents = f.read()
+        return file_contents
 
 def main():
-    get_word_count('./books/frankenstein.txt')
+    # book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        content = get_content(book_path)
+        num_words = get_word_count(content)
+        chars_dict = get_character_count(content)
+        chars_sorted = sort_dictionaries (chars_dict)
+        get_character_count (book_path)
+        print ("============ BOOKBOT ============")
+        print (f"Analyzing book found at {book_path}")
+        print ("----------- Word Count ----------")
+        print (f"Found {num_words} total words")
+        print ("--------- Character Count -------")
+        for character in chars_sorted:
+            print (character["char"] + ": " + str(character["count"]))
+        print ("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,30 @@
-def get_word_count(book):
-    with open(book) as f:
-        file_contents = f.read()
-        words= file_contents.split()
-        word_count = len(words)
-        print (f"{word_count} words found in the document")
+from collections import Counter
+
+def get_word_count(content):
+    words= content.split()
+    return len(words)
+
+def get_character_count(content):
+    character_count_dictionary={}
+    for c in content.lower():
+        if c not in character_count_dictionary:
+            character_count_dictionary[c] = 1
+        else:
+            character_count_dictionary[c] += 1
+    return character_count_dictionary
+
+def sort_on(items):
+    return items["count"]
+
+def sort_dictionaries(dictionary):
+    dictionary_list=[]
+    for character in dictionary:
+        if character.isalpha():
+            count = dictionary[character]
+            character_dictionary= {
+                "char": character,
+                "count": count
+            }
+            dictionary_list.append(character_dictionary)
+    dictionary_list.sort(reverse=True, key=sort_on)
+    return dictionary_list        
